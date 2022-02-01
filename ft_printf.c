704d04647f9715ddda49b24f305bd53ba32c73f3
Tamerlan BASKHANOV <tbaskhan@e2r8p16.clusters.42paris.fr>
2022-02-01 11:40:51 +0100

    100/100

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: tbaskhan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 08:18:37 by tbaskhan          #+#    #+#             */
-/*   Updated: 2022/01/31 15:51:31 by tbaskhan         ###   ########.fr       */
+/*   Updated: 2022/02/01 11:40:42 by tbaskhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,44 +63,4 @@ int	ft_printf(const char *str, ...)
 	}
 	va_end(args);
 	return (res);
-}
-
-#include "printf.h"
-#include <stdio.h>
-
-int	main()
-{
-	int	test_p;
-
-	test_p = 12;
-	char	*test = "salut";
-	char c = 'e';
-
-	printf("%d", printf("%d %i",-30,-30));
-	printf("\n \n \n");
-	ft_printf("%d", ft_printf("%d %i",-30,-30));
-
-	printf("\n \n \n \n \n \n");
-
-	ft_printf("===============Test p1\n");
-	ft_printf("%p\n", test);
-	printf("%p\n", test);
-	ft_printf("===============Test p2\n");
-	ft_printf("%p\n", &c);
-	printf("%p\n", &c);
-	ft_printf("=================Test s\n");
-	ft_printf("%s\n", "test de mon printf");
-	printf("%s\n", "test du vrai printf");
-	ft_printf("==================Test d\n");
-	ft_printf("%d\n", -30);
-	printf("%d\n", -30);
-	ft_printf("=================Test u\n");
-	ft_printf("%u\n", (unsigned int)789654123);
-	printf("%u\n", (unsigned int)789654123);
-	ft_printf("================Test x\n");
-	ft_printf("%x\n", 2147413647);
-	printf("%x\n", 2147413647);
-	ft_printf("================Test upx\n");
-	ft_printf("%X\n", 2147413647);
-	printf("%X\n", 2147413647);
 }
